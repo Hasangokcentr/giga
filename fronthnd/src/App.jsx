3635fc2a082318af70002b2f93c4daac3a17052f
Hasangokcentr <hasangkcn2005@gmail.com>
2025-11-29 11:59:32 +0300

    Dashboard harf hatasi ve App.jsx duzeltildi

--- fronthnd/src/App.jsx
+++ fronthnd/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage";
-import DashBoard from "./Pages/DashBoard";
+import Dashboard from "./Pages/Dashboard"; // <-- DİKKAT: Küçük 'b' ile
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Ana sayfa açılınca Login gelsin */}
         <Route path="/" element={<LoginPage />} />
-        <Route path="/dashBoard" element={<Dashboard />} />
+        
+        {/* Dashboard yolu küçük harfle olsun */}
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
